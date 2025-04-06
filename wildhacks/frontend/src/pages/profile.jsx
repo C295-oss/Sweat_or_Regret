539899443ca0c2d06a4c4eb905401f9c8bf72360
Csky_OS <charlie.bitsky@gmail.com>
2025-04-06 05:20:35 -0500

    added home.css

--- wildhacks/frontend/src/pages/profile.jsx
+++ wildhacks/frontend/src/pages/profile.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import React from "react";
+import './home.css'
 
 export default function Profile() {
+
+  // TODO: get actual account information
+  // TODO: consistent style
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+ 
   return (
-    <div className="profile">
-      <h1>Profile</h1>
-      <p>This is the profile page.</p>
-    </div>
+    <>
+
+      {/* Navbar: */}
+      <div>
+        <img className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0" src="frontend/src/assets/profile-user.png" alt="" />
+        <h1>My Profile:</h1>
+      </div>
+
+      <div className="h-px bg-violet-500 w-full my-4"></div>
+
+      {/* Personal Info: */}
+      <div>
+        <h1>Username: {username}</h1>
+        <h2>Password: {password}</h2>
+      </div>
+
+    </>
   );
 }
