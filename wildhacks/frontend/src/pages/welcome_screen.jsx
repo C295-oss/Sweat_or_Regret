ff371f912fbf074c48b7917d9109a0648933e045
SelomTetedje <selomdt52@gmail.com>
2025-04-06 03:40:57 -0500

    Matched up the welcome screen to match formmatting of home screen

--- wildhacks/frontend/src/pages/welcome_screen.jsx
+++ wildhacks/frontend/src/pages/welcome_screen.jsx
@@ -1,17 +1,30 @@
 import Button from '../components/ui/button';
+import './welcome.css';
 
 export default function WelcomePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
+    <div className="flex w-screen h-screen flex-col items-center justify-center p-2 bg-zinc-800">
       <div className="w-full max-w-md space-y-8 text-center">
-        <h1 className="text-4xl font-bold tracking-tight">Sweat of Regret</h1>
+        <h1 className="text-white font-bold text-2xl tracking-tight mb-2">Sweat of Regret</h1>
+        <br />
+        <div className="button-container">
+          <Button
+            label="Signup"
+            navigateTo="/registration"
+            className="text-violet-300 text-sm font-semibold uppercase tracking-wider"
+            size="lg"
+          />
+        </div>
 
-        <div className="flex flex-col space-y-4">
-          <Button label = "signup" navigateTo = "/registration" className="w-full" size="lg"/>
-          <Button label = "login" navigateTo = "/login" className="w-full" size="lg"/>
+        <div className="button-container">
+          <Button
+            label="Login"
+            navigateTo="/login"
+            className="text-violet-300 text-sm font-semibold uppercase tracking-wider"
+            size="lg"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
-
