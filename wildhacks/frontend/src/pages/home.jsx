38d531a5bd122704d723dc34f0b2703763fac703
Csky_OS <charlie.bitsky@gmail.com>
2025-04-05 19:33:42 -0500

    Dear god please don't break

--- wildhacks/frontend/src/pages/home.jsx
+++ wildhacks/frontend/src/pages/home.jsx
@@ -1,11 +1,34 @@
-import react  from "react";
+// import react from "react";
 
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Home</h1>
-      <p>This is the home page.</p>
+    <div class="flex flex-row m-0 p-0 w-screen h-screen">   
+      
+      {/* Stats */}
+      <div class="bg-red-500">
+        <h1 class="font-bold">Personal Stats</h1>
+          
+          <div class="border-2 rounded-2xl">
+            <h2>TODO: Stats go here...</h2>
+            <p>Strength: 6</p>
+            <p>Stamina/Endurance: 4</p>
+            <p>Agility: 2</p>
+            <p>Agility/Speed: 8</p>
+          </div>
+        
+      </div>
+
+      {/* display scenarios */}
+      <div class="bg-blue-400">
+
+        {/* <h1>Home</h1>
+        <p>This is the home page.</p> */}
+        <div class="bg-gray-800 text-green-800 rounded-2xl">
+          <h1>Ghjkl</h1>
+        </div>
+
+      </div>
     </div>
   );
 }
